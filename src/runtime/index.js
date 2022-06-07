095009257b6a2b38d34c466931a420345bac3ceb
liyi <584531510@qq.com>
2022-06-07 18:55:48 +0800

    feat: 函数导出

--- src/runtime/index.js
+++ src/runtime/index.js
@@ -1,5 +1,6 @@
 export { h, Text, Fragment } from './vnode'
 export { render } from './render'
 export { queueJob, nextTick } from './scheduler'
-export { createApp } from './createApp'
+export { createApp, resolveComponent } from './createApp'
 export { renderList } from './helpers/renderList'
+export { withModel } from './helpers/vModel'
